bcad0b5794c271fbbf75d1054e7d17392a2f1ec2
srushti06pattar <srushtipattar0623@gmail.com>
2026-05-06 22:18:27 +0530

    changed render url

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://realtime-chat-app-7dti.onrender.com/");
 
 function Chat() {
   const navigate = useNavigate();
